0c58a13a12675f6ebb39fb454f3bbc334b444c79
William Rocha <wrrocha@MacBook-Pro-de-William.local>
2025-04-18 10:08:06 -0300

    Updates

---
+++ precache-manifest.481fd3c2d2ed8272ca36677576c0de8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4067a10fa8acf6c1d2f7f777d35737c",
+    "url": "/english-word-trainer/index.html"
+  },
+  {
+    "revision": "a24238ef9c3292047750",
+    "url": "/english-word-trainer/static/css/main.d978e6a4.chunk.css"
+  },
+  {
+    "revision": "da8b8ee81106c8324f14",
+    "url": "/english-word-trainer/static/js/2.4519393a.chunk.js"
+  },
+  {
+    "revision": "a24238ef9c3292047750",
+    "url": "/english-word-trainer/static/js/main.ff3e04f1.chunk.js"
+  },
+  {
+    "revision": "655b5eb20be75d7cc226",
+    "url": "/english-word-trainer/static/js/runtime~main.2829b61a.js"
+  }
+]);
