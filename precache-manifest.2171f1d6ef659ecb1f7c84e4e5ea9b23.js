c0eeda4bf9e0b89bcc132ba9dc7ac782d342326e
William Rocha <wrrocha@MacBook-Pro-de-William.local>
2026-04-23 00:58:48 -0300

    Updates

---
+++ precache-manifest.2171f1d6ef659ecb1f7c84e4e5ea9b23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2964f00f445e7643fc28a20eaa70b09c",
+    "url": "/english-word-trainer/index.html"
+  },
+  {
+    "revision": "66687473acac446bdf31",
+    "url": "/english-word-trainer/static/css/main.db0f6c22.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/english-word-trainer/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "66687473acac446bdf31",
+    "url": "/english-word-trainer/static/js/main.221d7c52.chunk.js"
+  },
+  {
+    "revision": "655b5eb20be75d7cc226",
+    "url": "/english-word-trainer/static/js/runtime~main.2829b61a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/english-word-trainer/precache-manifest.0b897ff1088f0601a3c0b64d2ebe96ca.js"
+  "/english-word-trainer/precache-manifest.2171f1d6ef659ecb1f7c84e4e5ea9b23.js"
 );
 
 self.addEventListener('message', (event) => {
